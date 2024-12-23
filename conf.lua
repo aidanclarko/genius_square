c3ccfac0daa48a27824c7ac7ff2187789467a2e2
isaacclark0 <isaacsamuelclark@gmail.com>
2024-12-22 21:12:07 -0600

    stazrt

---
+++ conf.lua
@@ -0,0 +1,7 @@
+-- lua config or something?
+
+function love.conf(t)
+    t.title = "My Love2D Game"           -- Set the title of the window
+    t.window.width = 800                 -- Set the window width
+    t.window.height = 600                -- Set the window height
+end
